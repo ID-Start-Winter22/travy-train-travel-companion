9bc9b52f729e29d7403b77bf659d958ebe0e9cad
theopfr <teddypeifer@gmailc.com>
2022-11-11 16:57:08 +0100

    cleaned up rasa

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,12 +1,3 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 from pyparsing import nestedExpr
 
@@ -15,10 +6,6 @@ from rasa_sdk.executor import CollectingDispatcher
 import requests
 
 
-# NOTE(Michael): We could use this action to store the name in
-#                the TrackerStore (in memory database) or a persitent DB
-#                such as MySQL. But we need to store a key-value pair 
-#                to identify the user by id eg. (user_id, slotvalue)
 class ActionStoreUserName(Action):
 
      def name(self) -> Text:
@@ -44,6 +31,7 @@ class ActionUserName(Action):
             dispatcher.utter_message(' Du bist {}'.format(username))
 
         return []
+        
 
 class ActionPrintBahnData(Action):
 
