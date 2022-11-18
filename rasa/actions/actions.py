d0bf0b0277a219b386254f0918ecfcd78e1d89c7
theopfr <teddypeifer@gmailc.com>
2022-11-18 16:21:54 +0100

    created function to fetch data and implemented action to fetch and print train dats

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,19 +1,10 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 from pyparsing import nestedExpr
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-import requests
-
+from .api_functions import get_train_data
+import re
 
 class ActionPrintTrainId(Action):
     def name(self) -> Text:
@@ -24,19 +15,51 @@ class ActionPrintTrainId(Action):
         if not number:
             dispatcher.utter_message("Ich kenne deinen Zug noch nicht.")
         else:
-            dispatcher.utter_message(' Dein Zug ist {}'.format(number))
+            dispatcher.utter_message("Dein Zug ist {}.".format(number))
 
         return []
     
 
-class ActionStoreTrainId(Action):
-
+class ActionStoreTrainData(Action):
      def name(self) -> Text:
-         return "action_store_train_id"
+        return "action_store_train_data"
          
      def run(self, dispatcher, tracker, domain):
-        number = tracker.get_slot("train_id")
-        print("Sender ID: ", tracker.sender_id)
+        # old entity extraction using rasa, didn't work, gonna delete soon I swear
+        # train_id = tracker.get_slot("train_id")
+        # train_id = next(tracker.get_latest_entity_values("train_id"), None)
+
+        # get latest message sent by user
+        current_state = tracker.current_state()
+        latest_message = current_state["latest_message"]["text"]
+        
+        # find the train id provided by the user using regex, if not found stop function
+        regex_result = re.findall("((ECE|ICE|EC|IC|RE|THA|RJ|FLX|HBX|WB|D|EN|NJ|DN|IRE|MEX|RE|FEX|RB|S)(\s|)(\d{1,5}))", latest_message)
+        if len(regex_result) > 0:
+            train_id = regex_result[0][0]
+        else:
+            dispatcher.utter_message("Tut mit leid, ich habe nicht verstanden welchen Zug du nehmen möchtest!")
+            return []
+
+        # fetch data from bahn.expert given the train id
+        train_data = get_train_data(train_id)
+
+        # if the train is not found, tell user
+        if train_data == -1:
+            dispatcher.utter_message(f"Tut mir leid, ich konnte den Zug '{train_id}' nicht finden!")
+        else:
+            response_message = f"Alles klar, ich informiere dich über den Zug '{train_data['train']['name']}'!\nZugdaten:\n" \
+                                f" Von: {train_data['segmentStart']['title']}\n" \
+                                f" Nach: {train_data['segmentDestination']['title']}\n" \
+                                f" Abfahrtszeit: {train_data['departure']['scheduledTime']}\n" \
+                                f" Ankunftszeit: {train_data['arrival']['scheduledTime']}\n"
+
+            if "platform" in train_data["departure"]:
+                response_message += f" Gleis: {train_data['departure']['platform']}"
+
+            dispatcher.utter_message(response_message)
+
+        # TODO store initial data
 
         return []
 
