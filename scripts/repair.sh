743d411f71a058d42b15e7a3bdf8cf462a4b0124
cma5_win <hello@cma5.de>
2022-12-15 11:46:25 +0100

    additional comments

--- scripts/repair.sh
+++ scripts/repair.sh
@@ -1,3 +1,5 @@
+#get IP from commandline argument
+#if there's no argument, abort
 IP=$1
 if [ -z "$IP" ]
 then
@@ -6,7 +8,11 @@ then
 else
   echo "IP is set to '$1'"
 fi
+
+#replace an arbitrary IP with the IP provided from the commandline ($1)
 cd /var/www/html/
 sudo sed -i -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"$1"/ index.html
 sudo sed -i -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"$1"/ scripts.js
+
+#restart the html server
 sudo systemctl restart apache2
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,27 @@
+#setup.sh - run this after pulling from develop
+
+#import our IP ($ELIP)
+source ~/.env
+
+#shutdown the old rasa and the rasa-action server
 pkill screen
+
+#train rasa + run the rasa and the rasa action server
+#the rasa server puts its logs into /home/ubuntu/chat-team-11/rasa/out.log
 cd ~/chat-team-11/rasa
 conda activate rasaenv
 rasa train
 screen -S rasa -d -m rasa run --cors "*" --enable-api --log-file out.log --debug
 screen -S rasa-actions -d -m rasa run actions
+
+#copy everything [*], including directorys [-r] to the apache2 server
 cd ~/chat-team-11/frontend
 sudo cp -r * /var/www/html/
+
+#replace 'localhost' in the 'index.html' and 'scripts.html' with our server-IP
 cd /var/www/html/
-sudo sed -i -e "s/localhost/34.251.35.212/g" index.html
-sudo sed -i -e "s/localhost/34.251.35.212/g" scripts.js
+sudo sed -i -e "s/localhost/$ELIP/g" index.html
+sudo sed -i -e "s/localhost/$ELIP/g" scripts.js
+
+#restart html server
 sudo systemctl restart apache2
